f47ad47ee8f06e2917237d8fc1e8bce4435f9b1b
Your Name <you@example.com>
2023-08-03 14:53:29 +0530

    HEllo Ji

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component } from "@angular/core"
+import { CalculatorComponent } from "./calculator/calculator.component"
+import { EmployeeComponent } from "./employee/employee.component"
+import { InterestCalculationComponent } from "./interest-calculation/interest-calculation.component"
+import { PipeExampleComponent } from "./pipe-example/pipe-example.component"
+import { UserComponent } from "./user/user.component"
+import { NgModel } from "@angular/forms"
+import { CustomaerComponent } from './customaer/customaer.component';
 
-const routes: Routes = [];
+const routes: Routes=[
+  {path:'calculator',component:CalculatorComponent},
+  {path:'customer',component:CustomaerComponent},
+  {path:'employee',component:EmployeeComponent},
+  {path:'interest_calculation',component:InterestCalculationComponent},
+  {path:'pipe_example',component:PipeExampleComponent},
+  {path:'user',component:UserComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
